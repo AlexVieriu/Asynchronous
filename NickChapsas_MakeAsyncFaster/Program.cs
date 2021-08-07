1592f6c255673dc7daa3a123a38b5b34d50c6fca
vieriu.alexandru.25 <vieriu.alexandru.25@gmail.com>
2021-08-07 05:39:00 +0300

    commit

---
+++ NickChapsas_MakeAsyncFaster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NickChapsas_MakeAsyncFaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
